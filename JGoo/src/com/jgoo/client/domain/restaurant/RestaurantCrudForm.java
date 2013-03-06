977f9206b5665d891f2b4436f7cb5135fa022689
aglassman <andrew.glassman@gmail.com>
2013-03-06 17:41:35 -0600

    Updated form for Resturant example

--- JGoo/src/com/jgoo/client/domain/restaurant/RestaurantCrudForm.java
+++ JGoo/src/com/jgoo/client/domain/restaurant/RestaurantCrudForm.java
@@ -1,6 +1,8 @@
 package com.jgoo.client.domain.restaurant;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.shared.EventBus;
@@ -10,6 +12,7 @@ import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiTemplate;
 import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HasValue;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
@@ -30,11 +33,17 @@ public class RestaurantCrudForm extends Composite implements CrudForm{
 	private static Restaurant2 restaurant2 = GWT.create(Restaurant2.class);
 	
 	@UiField Label id;
-	@UiField TextBox nameVal;
-	@UiField TextBox descriptionVal;
-	@UiField TextBox todayVal;
+	@UiField TextBox name;
+	@UiField TextBox foodType;
+	@UiField TextBox orderOnline;
+	@UiField TextBox website;
+	@UiField TextBox phone;
+	@UiField TextBox address;
+	@UiField TextBox city;
+	@UiField TextBox state;
+	@UiField TextBox zip;
 	
-	
+	List<TextBox> fields;
 	
 	public RestaurantCrudForm(Class z)
 	{
@@ -50,7 +59,7 @@ public class RestaurantCrudForm extends Composite implements CrudForm{
 		{
 			throw new IllegalArgumentException("Not a valid binder class.");
 		}
-		
+		fields = Arrays.asList(name,foodType,orderOnline,website,phone,address,city,state,zip);
 	}
 
 
@@ -65,9 +74,8 @@ public class RestaurantCrudForm extends Composite implements CrudForm{
 	@Override
 	public void clearForm() {
 		id.setText(null);
-		nameVal.setText("");
-		descriptionVal.setText("");
-		todayVal.setText("");
+		for(TextBox b: fields)
+			b.setText("");
 	}
 
 
@@ -76,9 +84,15 @@ public class RestaurantCrudForm extends Composite implements CrudForm{
 	public void loadForm(CrudObject object) {
 		Restaurant restaurant = (Restaurant)object;
 		id.setText(restaurant.getId() == null ? "" : restaurant.getId());
-		nameVal.setText(restaurant.getName());
-		descriptionVal.setText(restaurant.getCity());
-		todayVal.setText(restaurant.getFoodType());
+		name.setText(restaurant.getName());
+		city.setText(restaurant.getCity());
+		foodType.setText(restaurant.getFoodType());
+		orderOnline.setText(restaurant.isOrderOnline() == null ? "" : restaurant.isOrderOnline().toString());
+		website.setText(restaurant.getWebsite());
+		phone.setText(restaurant.getPhone());
+		address.setText(restaurant.getStreetAddress());
+		state.setText(restaurant.getState());
+		zip.setText(restaurant.getZip());
 	}
 
 
@@ -87,9 +101,15 @@ public class RestaurantCrudForm extends Composite implements CrudForm{
 	public Restaurant getObjectFromForm() {
 		Restaurant r = new Restaurant();
 		r.setId(id.getText().equals("") ? null : id.getText());
-		r.setName(nameVal.getValue());
-		r.setCity(descriptionVal.getValue());
-		r.setFoodType(todayVal.getValue());
+		r.setName(name.getValue());
+		r.setCity(city.getValue());
+		r.setFoodType(foodType.getValue());
+		r.setOrderOnline(2l);
+		r.setWebsite(website.getText());
+		r.setPhone(phone.getText());
+		r.setStreetAddress(address.getText());
+		r.setState(state.getText());
+		r.setZip(zip.getText());
 		return r;
 	}
 
@@ -97,9 +117,8 @@ public class RestaurantCrudForm extends Composite implements CrudForm{
 
 	@Override
 	public void setEnabled(boolean b) {
-		nameVal.setEnabled(b);
-		descriptionVal.setEnabled(b);
-		todayVal.setEnabled(b);
+		for(TextBox box: fields)
+			box.setEnabled(b);
 	}
 
 
